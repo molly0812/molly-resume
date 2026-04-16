0be15de77372c03ddc97f4ac27bfd512414d8dbb
molly <65885362+molly0812@users.noreply.github.com>
2026-04-16 16:53:39 +0800

    fix: 右侧内容区增加顶部间距

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
       <div className="relative z-10 mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-16 lg:py-0">
         <div className="lg:flex lg:justify-between lg:gap-4">
           <Sidebar activeSection={activeSection} />
-          <main className="lg:w-[52%]">
+          <main className="lg:w-[52%] lg:pt-24">
             <About />
             <Experience />
             <Projects />
